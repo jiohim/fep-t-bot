9acc9d3454417f7e01f13f8eb503e2e1e31fd374
Jiohim <bohdankrasin@gmail.com>
2023-12-23 18:13:11 +0100

    initial commit

---
+++ src/main/java/pl/jiohim/DispatcherApplication.java
@@ -0,0 +1,17 @@
+package pl.jiohim;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import pl.jiohim.controller.TelegramBot;
+
+@SpringBootApplication
+public class DispatcherApplication {
+
+    public static void main(String[] args) throws TelegramApiException {
+        SpringApplication.run(DispatcherApplication.class);
+    }
+}
